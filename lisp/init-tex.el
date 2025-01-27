abf98cf95617c94b7a3c6ffa4ef11f7843fc0257
lemyx <dyi.shiou@gmail.com>
2025-01-27 13:59:09 +0800

    Enable outline-minor-mode for LaTeX

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -17,6 +17,25 @@
 
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; outline-minor-mode
+;; https://www.bilibili.com/video/BV1i4411a74k?vd_source=d2fb836a26e496b507775f8784008115&spm_id_from=333.788.videopod.sections
+(use-package outline
+  :init
+  (setq outline-minor-mode-prefix (kbd "C-'"))
+  :bind
+  (:map outline-minor-mode-map
+        ("C-' t" . outline-hide-body)
+        ("C-' e" . outline-show-entry)
+        ("C-' p" . outline-backward-same-level)
+        ("C-' n" . outline-forward-same-level)
+        ("C-' a" . outline-show-all))
+  :hook
+  (LaTeX-mode . (lambda ()
+		  (outline-minor-mode)
+		  (outline-hide-body)))
+  )
+
 (setq-default TeX-master nil)
 (setq TeX-global-PDF-mode t TeX-engine 'xetex)
 (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
