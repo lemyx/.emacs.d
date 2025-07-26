0d89c9b82129c913977608a664b4e1353a8b6d19
lemyx <xiudi@meituan.com>
2025-07-26 12:24:45 +0800

    配置 kimi-k2 驱动的 claude-code.el

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -3,18 +3,22 @@
 
 ;;; Code:
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; gptel
-(defun deepseek-api-key-path ()
-  "Return the path to the DeepSeek API key file."
-  (expand-file-name "~/.dotfiles/llm/deepseek/api_key.txt"))
-
 (defun read-llm-api-key (path)
   "Read and return the API key from the specified file path."
   (with-temp-buffer
     (insert-file-contents path)
     (string-trim (buffer-string))))
 
+(defun deepseek-api-key-path ()
+  "Return the path to the DeepSeek API key file."
+  (expand-file-name "~/.dotfiles/llm/api_keys/deepseek.txt"))
+
+(defun moonshot-api-key-path ()
+  "Return the path to the Moonshot API key file."
+  (expand-file-name "~/.dotfiles/llm/api_keys/moonshot.txt"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; gptel
 (use-package gptel
   :ensure t
   :config
@@ -44,16 +48,33 @@
   :ensure t
   :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
   :config
-  (progn
-    (setenv "ANTHROPIC_BASE_URL" "http://localhost:8082")
-    (setenv "ANTHROPIC_AUTH_TOKEN" "api-key")
-    (setq claude-code-program "/usr/local/bin/claude")
-    )
   (setq claude-code-terminal-backend 'eat)
   (setq claude-code-term-name "xterm-256color")
   (setq claude-code-program-switches '("--verbose"))
   (setq claude-code-enable-notifications t)
   (setq claude-code-notification-function 'claude-code--default-notification)
+
+  ;; 方法一: 通过 claude-code-proxy 的方式设置代理, 接入 OPENAI-compatible 模型
+  ;; 启动 claude-code-proxy 参考 https://github.com/sylearn/AICode 跟进该项目以保持服务可用
+  ;; (progn
+  ;;   (setenv "ANTHROPIC_BASE_URL" "http://192.168.31.174:8082")
+  ;;   (setenv "ANTHROPIC_AUTH_TOKEN" "api-key")
+  ;;   (setq claude-code-program "/usr/local/bin/claude")
+  ;;   )
+
+  ;; 方法二: 使用 kimi Anthropic-compatible API interface
+  ;; kimi 提供了 Anthropic-compatible API 接口, 参考链接 https://platform.moonshot.ai/docs/guide/agent-support.en-US#install-cline
+  ;; kimi 存在充值与限速问题, 参考链接 https://platform.moonshot.cn/docs/pricing/limits#%E9%99%90%E9%80%9F%E6%A6%82%E5%BF%B5%E8%A7%A3%E9%87%8A
+  ;; 可以关注 LLM-Red-Team/kimi-cc 项目中的讨论, 例如 https://github.com/LLM-Red-Team/kimi-cc/issues/35
+  ;; TODO: 探索这种使用方式下切换模型
+  (progn
+    (setenv "ANTHROPIC_BASE_URL" "https://api.moonshot.cn/anthropic/")
+    (setenv "ANTHROPIC_AUTH_TOKEN" (read-llm-api-key (moonshot-api-key-path)))
+    (setq claude-code-program "/usr/local/bin/claude")
+    )
+
+  ;; 方法三: 接入 Qwen3-Coder
+  ;; Qwen3-Coder 官方提供了接入 claude-code 和 qwen-code 的方式, 参考链接 https://qwenlm.github.io/blog/qwen3-coder/#claude-code
   )
 
 (one-key-create-menu
